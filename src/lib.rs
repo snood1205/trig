5849448e7c467a5df41c7543e76ba24dbef560e4
Eli Sadoff <snood1205@gmail.com>
2016-07-05 12:36:57 -0400

    Trig Sin

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+mod trig;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        assert!(trig::sin(0) == 0);
     }
 }
---
+++ src/trig.rs
@@ -0,0 +1,44 @@
+fn sin(x: f64) -> f64 {
+    let atol: f64 = 1e-12;
+    let mut diff: f64 = 1.0;
+    let mut sin: f64 = 0.0;
+    let mut i: u8 = 1;
+    while abs(diff) > atol {
+        diff = (sin_neg(i) as f64) * pow(x,i) / (fact(i) as f64);
+        sin += diff;
+        i += 2;
+    }
+    sin
+}
+
+fn abs(n: f64) -> f64 {
+    if (n as i64) < 0 {
+        n * -1.0
+    } else {
+        n
+    }
+}
+
+fn fact(z: u8) -> u64 {
+    if z == 0 {
+        1
+    } else {
+        (z as u64) * fact(z - 1)
+    }
+}
+
+fn pow(x: f64, n: u8) -> f64 {
+    if n == 0 {
+        1.0
+    } else {
+        x * pow(x, n - 1)
+    }
+}
+
+fn sin_neg(i: u8) -> i8 {
+    if i % 4 == 1 {
+        1
+    } else {
+        -1
+    }
+}
